89b178dc10a62eb0990b6bde248dbd0edd3efbe6
Paul Hudson <paulhudsonx@protonmail.com>
2023-05-13 19:17:11 +0100

    Add service implementation and test

---
+++ test/src/com/caci/bricks/orders/BrickOrderingServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.caci.bricks.orders;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BrickOrderingServiceImplTest {
+
+  @Test
+  void placeOrder() {
+  }
+
+  @Test
+  void findOrderDetails() {
+  }
+
+  @Test
+  void findAllOrderDetails() {
+  }
+
+  @Test
+  void updateOrder() {
+  }
+
+  @Test
+  void fulfilOrder() {
+  }
+}
